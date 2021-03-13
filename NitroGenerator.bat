69cf3e795f1ad53fce0c759d332b0f4c27de0a82
SSoloSkr1pts <78215150+SSoloSkr1pts@users.noreply.github.com>
2021-03-13 16:13:08 -0400

    Add files via upload

---
+++ NitroGenerator.bat
@@ -0,0 +1,100 @@
+@echo off
+cls
+title XBot Link And Nitro Generator By TT99
+color a
+echo --------------------------------------------
+echo XBot Link And Nitro Generator By TT99
+echo --------------------------------------------
+echo Date : %DATE%
+echo Time : %TIME%
+echo --------------------------------------------
+echo XBot Link
+echo --------------------------------------------
+echo Welcome........
+pause
+echo Generating A Link......
+echo ------------------------------------------------------------------------------------------
+echo Generated!
+pause
+color b
+echo https://www.google.com/search?q=xbot+tutorial+geometry+dash
+echo Copy And Paste It
+echo ----------------------------------------------------------------------------------------------
+echo If You Want To Generate Nitro, Press Any Button. If Not, Close This File.
+pause
+goto a
+:a
+echo -----------------------------------------------------------------------------------------------
+color c 
+echo Nitro Generator By TT99
+echo 𝕀ℕ𝕊𝕋ℝ𝕌ℂ𝕋𝕀𝕆ℕ𝕊
+echo Press Any Key To Generate Codes - To Stop, Click Anywhere On The Program.
+echo --------------
+echo Status : %random% Codes Available.
+echo ---------------
+echo Proxies Found.....
+pause
+echo Hang On While We Decrypt Them.
+pause
+echo Proxies = 𝕀ℕ𝕊𝕋ℝ𝕌ℂ𝕋𝕀𝕆ℕ𝕊𝕀ℕ𝕊𝕋ℝ𝕌ℂ𝕋𝕀𝕆ℕ𝕊𝕀ℕ𝕊𝕋ℝ𝕌ℂ𝕋𝕀𝕆ℕ𝕊
+echo Press Any Key To Start The Decryption.
+pause
+goto id1
+:id1
+echo Proxy = %random%.%random%.%random%
+goto id2
+:id2
+echo Proxy = %random%.%random%.%random%
+goto id3
+:id3
+echo Proxy = %random%.%random%.%random%
+goto id4
+:id4
+echo Proxy = %random%.%random%.%random%
+goto id5
+:id5
+echo Proxy = %random%.%random%.%random%
+goto decoration
+:decoration
+color a
+echo --------------------------------------------------------------------
+echo Proxies Successfully Generated!
+echo --------------------------------------------------------------------
+echo.
+pause
+echo -----------------------------------------------------------------------------------
+:top
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 0
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 1
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 2
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 3
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 4
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 5
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 6
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 7
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 8
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color 9
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color a
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color b
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color c
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color d
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color e
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+color f
+echo dicord.gift/%random%%random%%random%%random%%random%%random%%random%%random%
+goto top
